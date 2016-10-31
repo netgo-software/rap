fc913d6cf6a4c176c3d070f4255bc394f8287fb6
Frank Jakop <frank.jakop@arxes-tolina.de>
2016-10-31 12:15:25 +0100

    Revert "Delay syncSource, see Bug 474141"

--- tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/BrowserTest.js
+++ tests/org.eclipse.rap.rwt.jstest/js/org/eclipse/rwt/test/tests/BrowserTest.js
@@ -1,5 +1,9 @@
 /*******************************************************************************
+<<<<<<< streams/3.1-maintenance-ck-conf-lang
  * Copyright (c) 2010, 2016 EclipseSource and others.
+=======
+ * Copyright (c) 2010, 2014 EclipseSource and others.
+>>>>>>> 98a0f15 Sync URL of BrowserWidget with delay
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -67,7 +71,7 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.BrowserTest", {
       widget.destroy();
     },
 
-    testSetUrlByProtocol : [
+    testSetUrlByProtocol :  [
       function() {
         TestUtil.createShellByProtocol( "w2" );
         Processor.processOperation( {
@@ -76,22 +80,12 @@ rwt.qx.Class.define( "org.eclipse.rwt.test.tests.BrowserTest", {
           "type" : "rwt.widgets.Browser",
           "properties" : {
             "style" : [],
-            "parent" : "w2"
-          }
-        } );
-        TestUtil.flush();
-
-        Processor.processOperation( {
-          "target" : "w3",
-          "action" : "set",
-          "properties" : {
+            "parent" : "w2",
             "url" : this.URL1
           }
         } );
-
-        var browser = ObjectRegistry.getObject( "w3" );
-        assertEquals( browser.getIframeNode().src.slice( -10 ), "blank.html" );
         TestUtil.delayTest( 7000 );
+        var browser = ObjectRegistry.getObject( "w3" );
         TestUtil.store( browser );
       },
       function( browser ) {
